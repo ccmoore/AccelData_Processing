ad4e35dfffaf2ed09ebcbd1d660a6c5e962a732b
brentscott93 <bdscott@umass.edu>
2018-12-09 15:25:32 -0500

    update to function mvpa_plot. better code

--- Accelerometer/R/mvpa_plot.R
+++ Accelerometer/R/mvpa_plot.R
@@ -9,19 +9,15 @@
 #'
 #' @examples weekend_day_graph(subject1_data, S1)
 mvpa_plot <- function(dat, subject){
-  x <- ggplot(dat)+
-  geom_col(aes(x = weekday_classification, y = Average_MVPA, fill = weekday_classification))+
-  scale_fill_manual(values = c("black", "gray"))+
-  xlab("Weekday Classification")+
-  ylab("Average MVPA")+
-  ggtitle(subject)+
-  theme_bw()
-
-
-
-x2 <- x + geom_errorbar(data = example4, aes(x = weekday_classification, ymin = Average_MVPA- se, ymax = Average_MVPA + se))
-x3 <- x2 + guides(fill = guide_legend(title = "Weekday Classification"))
-x3
+  ggplot(dat)+
+    geom_col(aes(x = weekday_classification, y = Average_MVPA, fill = weekday_classification))+
+    scale_fill_manual(values = c("black", "gray"))+
+    geom_errorbar(aes(x = weekday_classification, ymin = Average_MVPA - se, ymax = Average_MVPA + se))+
+    guides(fill = guide_legend(title = "Weekday Classification"))+
+    xlab("Weekday Classification")+
+    ylab("Average MVPA")+
+    ggtitle(subject)+
+    theme_bw()
 }
 
 
